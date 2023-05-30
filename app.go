2c8f0117ee8299cfbdbd9183eab6d2847997bbe1
osbits <foundation@osbits.io>
2023-05-30 17:41:18 +0300

    Validation.

--- app.go
+++ app.go
@@ -2,19 +2,20 @@ package gorgany
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/spf13/viper"
 	"net/http"
 	"os"
-	"os/exec"
+	"path"
 	"strings"
 	"time"
 )
 
 var ServerTimezone *time.Location
-
-const ValidationSuccessMsg = "Successfully validated!"
 
 type App struct {
 	httpServer *http.Server
@@ -28,6 +29,7 @@ func (s *App) Run(router *chi.Mux, port string) error {
 		}
 	}
 
+	fmt.Println(port)
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
 		Handler:        router,
@@ -44,15 +46,60 @@ func (s *App) Shutdown(ctx context.Context) error {
 }
 
 func (s *App) validate() error {
-	dir, err := os.Getwd()
+	lock, err := s.parseLock()
 	if err != nil {
 		return err
 	}
-	fmtCmd := exec.Command("./grg", "validate-project", "-path", dir)
-	res, err := fmtCmd.Output()
-	resStr := string(res)
-	if !strings.Contains(resStr, ValidationSuccessMsg) {
-		return fmt.Errorf(resStr)
+
+	success := true
+	for _, file := range lock.Files {
+		content, err := os.ReadFile(path.Join(file.Path, file.Name))
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file or directory") {
+				return err
+			}
+			return err
+		}
+		checksum := md5.Sum(content)
+		checksumStr := hex.EncodeToString(checksum[:])
+		if checksumStr != file.Checksum {
+			success = false
+			fmt.Printf("File %s(%s) has been modified and the checksum does not match the generated copy!\n", file.Name, file.Path)
+		}
 	}
+
+	if !success {
+		return fmt.Errorf("Validation error")
+	}
+
 	return nil
+}
+
+type Lock struct {
+	Name    string
+	Version int `json:"lockfileVersion"`
+	Files   []LockFiles
+}
+
+type LockFiles struct {
+	Name     string
+	Path     string
+	Checksum string
+}
+
+func (s *App) parseLock() (*Lock, error) {
+	content, err := os.ReadFile("grg-lock.json")
+	if err != nil {
+		if strings.Contains(err.Error(), "no such file or directory") {
+			return &Lock{}, nil
+		}
+		return nil, err
+	}
+
+	lock := new(Lock)
+	err = json.Unmarshal(content, lock)
+	if err != nil {
+		return nil, err
+	}
+	return lock, nil
 }
