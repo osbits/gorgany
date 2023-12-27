d779239068412aab80b35955969f8fe51db0f349
osbits <foundation@osbits.io>
2023-12-27 12:23:24 +0200

    Fix multipart parses when value is blank.

--- decoder/multipart/multipart_values.go
+++ decoder/multipart/multipart_values.go
@@ -2,6 +2,7 @@ package multipart
 
 import (
 	"git.qix.sx/gorgany/gorgany.git/model"
+	"git.qix.sx/gorgany/gorgany.git/util"
 	"github.com/gorilla/schema"
 	"reflect"
 	"strings"
@@ -38,11 +39,14 @@ func NewFormValuesDecoder() *ValuesDecoder {
 		return reflect.ValueOf(model.FormDateLocal{Time: t})
 	})
 
+	decoder.IgnoreUnknownKeys(true)
+
 	return &ValuesDecoder{formSchemaDecoder: decoder}
 }
 
 func (thiz ValuesDecoder) Decode(dst interface{}, src map[string][]string) error {
 	reflectedValue := reflect.ValueOf(dst)
+	reflectedType := reflect.TypeOf(dst)
 
 	mapValues := make(map[string]map[string]string)
 	for key, values := range src {
@@ -61,6 +65,32 @@ func (thiz ValuesDecoder) Decode(dst interface{}, src map[string][]string) error
 	for key, values := range mapValues {
 		reflectedField := reflectedValue.Elem().FieldByName(key)
 		reflectedField.Set(reflect.ValueOf(values))
+	}
+
+	for key, values := range src {
+		if len(values) > 1 {
+			continue
+		}
+
+		reflectedField := reflectedValue.Elem().FieldByName(key)
+		if !reflectedField.IsValid() {
+			continue
+		}
+
+		if reflectedField.Type().Kind() != reflect.Ptr {
+			continue
+		}
+
+		val := values[0]
+		rv := reflect.ValueOf(val)
+		if !rv.IsZero() {
+			continue
+		}
+
+		reflectedFieldType, _ := util.IndirectType(reflectedType).FieldByName(key)
+		reflectedField.Set(reflect.Zero(reflectedFieldType.Type))
+
+		delete(src, key)
 	}
 
 	return thiz.formSchemaDecoder.Decode(dst, src)
