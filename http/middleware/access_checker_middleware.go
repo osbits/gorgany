646af2c5d70caf50d7662814c408424743d31eec
osbits <foundation@osbits.io>
2024-02-12 12:49:58 +0200

    Fix npe in access checker middleware.

--- http/middleware/access_checker_middleware.go
+++ http/middleware/access_checker_middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"git.qix.sx/gorgany/gorgany.git/app/core"
+	"git.qix.sx/gorgany/gorgany.git/log"
 	"git.qix.sx/gorgany/gorgany.git/service/dto"
 	"git.qix.sx/gorgany/gorgany.git/util"
 	"reflect"
@@ -19,6 +20,11 @@ func (thiz AccessCheckerMiddleware) Handle(message core.HttpMessage) bool {
 			continue
 		}
 		accessCheckerCommand = arg.Interface().(core.HttpAccessCommand)
+	}
+
+	if accessCheckerCommand == nil {
+		log.Log().Warnf("AccessCheckerMiddleware is enabled for \u001B[0;32m%s\u001B[0m, but instance of \u001B[0;33mcore.HttpAccessCommand\u001B[0m is not injected to handler", message.GetRequest().URL.Path)
+		return true
 	}
 
 	allowed := accessCheckerCommand.IsAccessAllowed(message.Context())
