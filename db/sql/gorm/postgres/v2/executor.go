9c3169f1cc2127da3fc69ea14388fa60795a880e
osbits <foundation@osbits.io>
2025-05-29 18:54:14 +0200

    Improve executor

--- db/sql/gorm/postgres/v2/executor.go
+++ db/sql/gorm/postgres/v2/executor.go
@@ -3,8 +3,9 @@ package v2
 import (
 	"context"
 	"fmt"
-	"git.qix.sx/gorgany/gorgany.git/db/sql/core"
 	"strings"
+
+	"git.qix.sx/gorgany/gorgany.git/db/sql/core"
 
 	"gorm.io/gorm"
 )
@@ -41,7 +42,7 @@ func (e *Executor) Count(ctx context.Context, query *core.Query) (int64, error) 
 
 // ExecuteRaw executes a raw SQL query without returning results
 func (e *Executor) ExecuteRaw(ctx context.Context, sql string, args ...interface{}) error {
-	return e.db.Raw(sql, args...).Exec("").Error
+	return e.db.Exec(sql, args...).Error
 }
 
 // ExecuteRawWithResult executes a raw SQL query and stores the results in the provided destination
@@ -58,90 +59,99 @@ func (e *Executor) CountRaw(ctx context.Context, sql string, args ...interface{}
 
 // buildGormQuery converts our Query to a GORM query
 func (e *Executor) buildGormQuery(query *core.Query) *gorm.DB {
-	gormQuery := e.db
+	var sqlBuilder strings.Builder
+	var args []interface{}
+	argIndex := 1
 
 	// Add CTEs if any
 	if len(query.CTEs) > 0 {
+		sqlBuilder.WriteString("WITH ")
 		cteParts := make([]string, len(query.CTEs))
 		for i, cte := range query.CTEs {
-			cteParts[i] = fmt.Sprintf("%s AS (%s)", cte.Name, cte.Query)
+			cteSQL, cteArgs := buildSubquery(cte.Query, &argIndex)
+			cteParts[i] = fmt.Sprintf("%s AS (%s)", cte.Name, cteSQL)
+			args = append(args, cteArgs...)
 		}
-		gormQuery = gormQuery.Raw("WITH " + strings.Join(cteParts, ", "))
+		sqlBuilder.WriteString(strings.Join(cteParts, ", "))
+		sqlBuilder.WriteString(" ")
 	}
 
 	// Add SELECT clause
 	if query.Select != nil {
-		var selectClause string
 		if query.Select.Distinct {
 			if len(query.Select.DistinctOn) > 0 {
-				selectClause = fmt.Sprintf("SELECT DISTINCT ON (%s) %s",
+				sqlBuilder.WriteString(fmt.Sprintf("SELECT DISTINCT ON (%s) %s",
 					strings.Join(query.Select.DistinctOn, ", "),
-					strings.Join(query.Select.Fields, ", "))
+					strings.Join(query.Select.Fields, ", ")))
 			} else {
-				selectClause = fmt.Sprintf("SELECT DISTINCT %s",
-					strings.Join(query.Select.Fields, ", "))
+				sqlBuilder.WriteString(fmt.Sprintf("SELECT DISTINCT %s",
+					strings.Join(query.Select.Fields, ", ")))
 			}
 		} else {
-			selectClause = fmt.Sprintf("SELECT %s",
-				strings.Join(query.Select.Fields, ", "))
+			sqlBuilder.WriteString(fmt.Sprintf("SELECT %s",
+				strings.Join(query.Select.Fields, ", ")))
 		}
-		gormQuery = gormQuery.Raw(selectClause)
 	}
 
 	// Add FROM clause
 	if query.From != nil {
+		sqlBuilder.WriteString(" FROM ")
 		if query.From.IsSubquery {
-			gormQuery = gormQuery.Raw(fmt.Sprintf("FROM (%s) AS %s",
-				query.From.Subquery,
-				query.From.Alias))
+			subquerySQL, subqueryArgs := buildSubquery(query.From.Subquery, &argIndex)
+			sqlBuilder.WriteString(fmt.Sprintf("(%s) AS %s", subquerySQL, query.From.Alias))
+			args = append(args, subqueryArgs...)
 		} else {
-			gormQuery = gormQuery.Raw(fmt.Sprintf("FROM %s",
-				query.From.Table))
+			sqlBuilder.WriteString(query.From.Table)
 		}
 	}
 
 	// Add JOINs
 	for _, join := range query.Joins {
-		var joinClause string
+		sqlBuilder.WriteString(fmt.Sprintf(" %s JOIN ", join.Type))
 		if join.IsSubquery {
-			joinClause = fmt.Sprintf("%s JOIN (%s) AS %s",
-				join.Type,
-				join.Subquery,
-				join.Alias)
+			subquerySQL, subqueryArgs := buildSubquery(join.Subquery, &argIndex)
+			sqlBuilder.WriteString(fmt.Sprintf("(%s) AS %s", subquerySQL, join.Alias))
+			args = append(args, subqueryArgs...)
 		} else {
-			joinClause = fmt.Sprintf("%s JOIN %s",
-				join.Type,
-				join.Table)
+			sqlBuilder.WriteString(join.Table)
 		}
 
 		if join.Condition != nil {
-			sql, _ := join.Condition.ToSQL()
-			joinClause += fmt.Sprintf(" ON %s", sql)
+			conditionSQL, conditionArgs := join.Condition.ToSQL()
+			sqlBuilder.WriteString(fmt.Sprintf(" ON %s", conditionSQL))
+			args = append(args, conditionArgs...)
 		}
-
-		gormQuery = gormQuery.Raw(joinClause)
 	}
 
 	// Add WHERE clause
 	if query.Where != nil {
-		whereSQL, _ := query.Where.ToSQL()
-		gormQuery = gormQuery.Raw(fmt.Sprintf("WHERE %s", whereSQL))
+		whereSQL, whereArgs := query.Where.ToSQL()
+		if whereSQL != "" {
+			sqlBuilder.WriteString(" WHERE ")
+			sqlBuilder.WriteString(whereSQL)
+			args = append(args, whereArgs...)
+		}
 	}
 
 	// Add GROUP BY clause
 	if query.GroupBy != nil {
-		gormQuery = gormQuery.Raw(fmt.Sprintf("GROUP BY %s",
+		sqlBuilder.WriteString(fmt.Sprintf(" GROUP BY %s",
 			strings.Join(query.GroupBy.Fields, ", ")))
 	}
 
 	// Add HAVING clause
 	if query.Having != nil {
-		havingSQL, _ := query.Having.ToSQL()
-		gormQuery = gormQuery.Raw(fmt.Sprintf("HAVING %s", havingSQL))
+		havingSQL, havingArgs := query.Having.ToSQL()
+		if havingSQL != "" {
+			sqlBuilder.WriteString(" HAVING ")
+			sqlBuilder.WriteString(havingSQL)
+			args = append(args, havingArgs...)
+		}
 	}
 
 	// Add ORDER BY clause
 	if query.OrderBy != nil {
+		sqlBuilder.WriteString(" ORDER BY ")
 		orderByParts := make([]string, len(query.OrderBy.Fields))
 		for i, field := range query.OrderBy.Fields {
 			if field.Direction != "" {
@@ -150,34 +160,141 @@ func (e *Executor) buildGormQuery(query *core.Query) *gorm.DB {
 				orderByParts[i] = field.Field
 			}
 		}
-		gormQuery = gormQuery.Raw(fmt.Sprintf("ORDER BY %s",
-			strings.Join(orderByParts, ", ")))
+		sqlBuilder.WriteString(strings.Join(orderByParts, ", "))
 	}
 
 	// Add LIMIT clause
 	if query.Limit != nil {
-		gormQuery = gormQuery.Raw(fmt.Sprintf("LIMIT %d", *query.Limit))
+		sqlBuilder.WriteString(fmt.Sprintf(" LIMIT %d", *query.Limit))
 	}
 
 	// Add OFFSET clause
 	if query.Offset != nil {
-		gormQuery = gormQuery.Raw(fmt.Sprintf("OFFSET %d", *query.Offset))
+		sqlBuilder.WriteString(fmt.Sprintf(" OFFSET %d", *query.Offset))
 	}
 
 	// Add UNION clauses
 	for _, union := range query.Unions {
 		if union.All {
-			gormQuery = gormQuery.Raw(fmt.Sprintf("UNION ALL %s", union.Query))
+			sqlBuilder.WriteString(" UNION ALL ")
 		} else {
-			gormQuery = gormQuery.Raw(fmt.Sprintf("UNION %s", union.Query))
+			sqlBuilder.WriteString(" UNION ")
 		}
+		unionSQL, unionArgs := buildSubquery(union.Query, &argIndex)
+		sqlBuilder.WriteString(unionSQL)
+		args = append(args, unionArgs...)
 	}
 
 	// Add RETURNING clause
 	if len(query.Returning) > 0 {
-		gormQuery = gormQuery.Raw(fmt.Sprintf("RETURNING %s",
+		sqlBuilder.WriteString(fmt.Sprintf(" RETURNING %s",
 			strings.Join(query.Returning, ", ")))
 	}
 
-	return gormQuery
+	return e.db.Raw(sqlBuilder.String(), args...)
+}
+
+// buildSubquery builds a subquery and returns its SQL and arguments
+func buildSubquery(query *core.Query, argIndex *int) (string, []interface{}) {
+	var sqlBuilder strings.Builder
+	var args []interface{}
+
+	// Add SELECT clause
+	if query.Select != nil {
+		if query.Select.Distinct {
+			if len(query.Select.DistinctOn) > 0 {
+				sqlBuilder.WriteString(fmt.Sprintf("SELECT DISTINCT ON (%s) %s",
+					strings.Join(query.Select.DistinctOn, ", "),
+					strings.Join(query.Select.Fields, ", ")))
+			} else {
+				sqlBuilder.WriteString(fmt.Sprintf("SELECT DISTINCT %s",
+					strings.Join(query.Select.Fields, ", ")))
+			}
+		} else {
+			sqlBuilder.WriteString(fmt.Sprintf("SELECT %s",
+				strings.Join(query.Select.Fields, ", ")))
+		}
+	}
+
+	// Add FROM clause
+	if query.From != nil {
+		sqlBuilder.WriteString(" FROM ")
+		if query.From.IsSubquery {
+			subquerySQL, subqueryArgs := buildSubquery(query.From.Subquery, argIndex)
+			sqlBuilder.WriteString(fmt.Sprintf("(%s) AS %s", subquerySQL, query.From.Alias))
+			args = append(args, subqueryArgs...)
+		} else {
+			sqlBuilder.WriteString(query.From.Table)
+		}
+	}
+
+	// Add JOINs
+	for _, join := range query.Joins {
+		sqlBuilder.WriteString(fmt.Sprintf(" %s JOIN ", join.Type))
+		if join.IsSubquery {
+			subquerySQL, subqueryArgs := buildSubquery(join.Subquery, argIndex)
+			sqlBuilder.WriteString(fmt.Sprintf("(%s) AS %s", subquerySQL, join.Alias))
+			args = append(args, subqueryArgs...)
+		} else {
+			sqlBuilder.WriteString(join.Table)
+		}
+
+		if join.Condition != nil {
+			conditionSQL, conditionArgs := join.Condition.ToSQL()
+			sqlBuilder.WriteString(fmt.Sprintf(" ON %s", conditionSQL))
+			args = append(args, conditionArgs...)
+		}
+	}
+
+	// Add WHERE clause
+	if query.Where != nil {
+		whereSQL, whereArgs := query.Where.ToSQL()
+		if whereSQL != "" {
+			sqlBuilder.WriteString(" WHERE ")
+			sqlBuilder.WriteString(whereSQL)
+			args = append(args, whereArgs...)
+		}
+	}
+
+	// Add GROUP BY clause
+	if query.GroupBy != nil {
+		sqlBuilder.WriteString(fmt.Sprintf(" GROUP BY %s",
+			strings.Join(query.GroupBy.Fields, ", ")))
+	}
+
+	// Add HAVING clause
+	if query.Having != nil {
+		havingSQL, havingArgs := query.Having.ToSQL()
+		if havingSQL != "" {
+			sqlBuilder.WriteString(" HAVING ")
+			sqlBuilder.WriteString(havingSQL)
+			args = append(args, havingArgs...)
+		}
+	}
+
+	// Add ORDER BY clause
+	if query.OrderBy != nil {
+		sqlBuilder.WriteString(" ORDER BY ")
+		orderByParts := make([]string, len(query.OrderBy.Fields))
+		for i, field := range query.OrderBy.Fields {
+			if field.Direction != "" {
+				orderByParts[i] = fmt.Sprintf("%s %s", field.Field, field.Direction)
+			} else {
+				orderByParts[i] = field.Field
+			}
+		}
+		sqlBuilder.WriteString(strings.Join(orderByParts, ", "))
+	}
+
+	// Add LIMIT clause
+	if query.Limit != nil {
+		sqlBuilder.WriteString(fmt.Sprintf(" LIMIT %d", *query.Limit))
+	}
+
+	// Add OFFSET clause
+	if query.Offset != nil {
+		sqlBuilder.WriteString(fmt.Sprintf(" OFFSET %d", *query.Offset))
+	}
+
+	return sqlBuilder.String(), args
 }
